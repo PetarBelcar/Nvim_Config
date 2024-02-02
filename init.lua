039dffce5dcc45c83621998c394354e63cc323bd
Petar Belcar <petar.belcar@gmail.com>
2024-02-02 16:41:52 +0100

    Added crates plugin for rust development

--- init.lua
+++ init.lua
@@ -230,10 +230,16 @@ require('lazy').setup({
     init = function ()
      vim.g.rustftm_autosave = 1
     end
-  }
-
-  
-
+  },
+  {
+    'saecki/crates.nvim',
+    ft = {"rust", "toml"},
+    config = function(_, opts)
+      local crates = require('crates')
+      crates.setup(opts)
+      crates.show()
+    end,
+  },
 
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
